24167f4f068d1e562a34d88afc19c184cfd0c27a
JouryABDULLAH <jours-222@outlook.com>
2024-02-03 09:01:51 -0800

    link rules page to services page

---
+++ lib/presentation/Servecis/rules/rules_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class rulesPage extends StatefulWidget {
+  const rulesPage({super.key});
+
+  @override
+  State<rulesPage> createState() => _rulesPageState();
+}
+
+class _rulesPageState extends State<rulesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
