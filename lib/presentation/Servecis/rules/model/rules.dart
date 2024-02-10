3d698c8408f3c2d65b861a408e638f85146b4513
JouryABDULLAH <jours-222@outlook.com>
2024-02-10 10:38:12 -0800

    show rules data in rules screen

--- lib/presentation/Servecis/rules/model/rules.dart
+++ lib/presentation/Servecis/rules/model/rules.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 class Rule {
   final String title;
   final String description;
-  final String steps;
+  final List<String> steps;
   bool isExpanded;
 
   Rule({
@@ -11,15 +14,54 @@ class Rule {
     this.isExpanded = false,
   });
 
-  static List<Rule> fetchAll() {
-    return [
-      Rule(title: 'title1', description: 'description1', steps: 'steps1'),
-      Rule(title: 'title1', description: 'description1', steps: 'steps1'),
-      Rule(title: 'title1', description: 'description1', steps: 'steps1'),
-      Rule(title: 'title1', description: 'description1', steps: 'steps1'),
-      Rule(title: 'title1', description: 'description1', steps: 'steps1'),
-      Rule(title: 'title1', description: 'description1', steps: 'steps1'),
-      Rule(title: 'title1', description: 'description1', steps: 'steps1'),
-    ];
+  static Future<List<Rule>> fetchAll() async {
+    // Read the JSON file from the file system
+    File file = File(
+        'lib/shared/data/data_encoded.json'); // Replace 'path_to_your_shared_folder' with the actual path
+    String jsonData = await file.readAsString();
+
+    // Parse the JSON data
+    List<dynamic> jsonList = json.decode(jsonData);
+
+    // Convert JSON data into a list of Rule objects
+    List<Rule> rules = jsonList.map((json) {
+      List<String> steps = List<String>.from(json['steps']);
+      return Rule(
+        title: json['title'],
+        description: json['description'],
+        steps: steps,
+      );
+    }).toList();
+    return rules;
   }
 }
+
+
+
+// import 'dart:convert';
+
+// class Rule {
+//   final String title;
+//   final String description;
+//   final String steps;
+//   bool isExpanded;
+
+//   Rule({
+//     required this.title,
+//     required this.description,
+//     required this.steps,
+//     this.isExpanded = false,
+//   });
+
+//   static List<Rule> fetchAll() {
+//     return [
+//       Rule(title: 'title1', description: 'description1', steps: 'steps1'),
+//       Rule(title: 'title1', description: 'description1', steps: 'steps1'),
+//       Rule(title: 'title1', description: 'description1', steps: 'steps1'),
+//       Rule(title: 'title1', description: 'description1', steps: 'steps1'),
+//       Rule(title: 'title1', description: 'description1', steps: 'steps1'),
+//       Rule(title: 'title1', description: 'description1', steps: 'steps1'),
+//       Rule(title: 'title1', description: 'description1', steps: 'steps1'),
+//     ];
+//   }
+// }
--- lib/presentation/Servecis/rules/rules_page.dart
+++ lib/presentation/Servecis/rules/rules_page.dart
@@ -15,6 +15,13 @@ class rulesPage extends StatefulWidget {
 // ignore: camel_case_types
 class _rulesPageState extends State<rulesPage> {
   @override
+  void initState() {
+    super.initState();
+    // Fetch the rules data when the screen is initialized
+    fetchRulesData();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -60,8 +67,20 @@ class _rulesPageState extends State<rulesPage> {
       ),
     );
   }
+
+  void fetchRulesData() async {
+    // Call the fetchAll method of the Rule class to fetch the data
+    List<Rule> fetchedRules = await Rule.fetchAll();
+    // Update the state with the fetched data
+    setState(() {
+      rules = fetchedRules;
+    });
+  }
 }
 
+List<Rule> rules = [];
+
+//-------------------------------------------------
 // stores ExpansionPanel state information
 
 // class Rule {
@@ -92,7 +111,7 @@ class _rulesPageState extends State<rulesPage> {
 //   // Add more rules as needed
 // ];
 
-final rules = Rule.fetchAll();
+//final rules = Rule.fetchAll();
 
 // List<Rule> academicRules() {
 //   return List<Rule>.from(rules);
@@ -154,26 +173,29 @@ class _RulesListState extends State<RulesList> {
                     255, 9, 55, 92), // Customize the color of the divider
               ),
               ListTile(
-                  hoverColor: ColorManager.blue,
-                  title: Text(rule.description),
-                  // هنا شرح الخدمة
-                  // subtitle: const Text(
-                  //     'هنا نجد تفاصيل الخدمة وتوضيحها وماهيتها الخ الخ الخ الخ الخ الخ ...'),
-                  trailing: const Column(
-                    children: [
-                      Icon(Icons.video_camera_back),
-                      Text('شرح الخدمة')
-                    ],
-                  ),
-                  onTap: () {
-                    // ignore: avoid_print
-                    print(rule.steps);
-                    // setState(() {
-                    //   // show the video on youtube
-                    //   _data.removeWhere(
-                    //       (Rule currentItem) => rule == currentItem);
-                    // });
-                  }),
+                hoverColor: ColorManager.blue,
+                title: Text(rule.title),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(rule.description),
+                    const SizedBox(
+                        height:
+                            8), // Add some space between description and steps
+                    Text('خطوات تنفيذ الخدمة:',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    for (String step in rule.steps)
+                      Padding(
+                        padding: const EdgeInsets.only(right: 16),
+                        child: Text(step),
+                      ),
+                  ],
+                ),
+                trailing: const Icon(Icons.list),
+                onTap: () {
+                  // Handle onTap event
+                },
+              ),
             ],
           ),
           isExpanded: rule.isExpanded,
@@ -181,4 +203,59 @@ class _RulesListState extends State<RulesList> {
       }).toList(),
     );
   }
+
+  // Widget _buildPanel() {
+  //   return ExpansionPanelList(
+  //     expandedHeaderPadding: const EdgeInsets.all(2),
+  //     animationDuration: const Duration(seconds: 1),
+  //     expansionCallback: (int index, bool isExpanded) {
+  //       setState(() {
+  //         _data[index].isExpanded = isExpanded;
+  //       });
+  //     },
+  //     children: _data.map<ExpansionPanel>((Rule rule) {
+  //       return ExpansionPanel(
+  //         canTapOnHeader: true,
+  //         backgroundColor: ColorManager.card,
+  //         headerBuilder: (BuildContext context, bool isExpanded) {
+  //           return ListTile(
+  //             title: Text(rule.title),
+  //           );
+  //         },
+  //         body: Column(
+  //           crossAxisAlignment: CrossAxisAlignment.stretch,
+  //           children: [
+  //             Container(
+  //               height: 1, // Adjust the height of the divider
+  //               color: const Color.fromARGB(
+  //                   255, 9, 55, 92), // Customize the color of the divider
+  //             ),
+  //             ListTile(
+  //                 hoverColor: ColorManager.blue,
+  //                 title: Text(rules[index].title),
+  //                 subtitle: Text(rules[index].description),
+  //                 // هنا شرح الخدمة
+  //                 // subtitle: const Text(
+  //                 //     'هنا نجد تفاصيل الخدمة وتوضيحها وماهيتها الخ الخ الخ الخ الخ الخ ...'),
+  //                 trailing: const Column(
+  //                   children: [Icon(Icons.list), Text('خطوات تنفيذ الخدمة')],
+  //                 ),
+  //                 onTap: () {
+  //                   // ignore: avoid_print
+  //                   subtitle:
+  //                   const Text(rule.steps);
+  //                   print(rule.steps);
+  //                   // setState(() {
+  //                   //   // show the video on youtube
+  //                   //   _data.removeWhere(
+  //                   //       (Rule currentItem) => rule == currentItem);
+  //                   // });
+  //                 }),
+  //           ],
+  //         ),
+  //         isExpanded: rule.isExpanded,
+  //       );
+  //     }).toList(),
+  //   );
+  // }
 }
