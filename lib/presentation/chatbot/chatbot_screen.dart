10573781c6256df15019c5bd85ee2bf2d40455ff
JouryABDULLAH <jours-222@outlook.com>
2024-02-11 18:09:09 -0800

    finish ui for chatbot welcom screen, and initialized the ui of the chat

--- lib/presentation/chatbot/chatbot_screen.dart
+++ lib/presentation/chatbot/chatbot_screen.dart
@@ -1,5 +1,10 @@
-import 'package:flutter/foundation.dart';
+// ignore_for_file: camel_case_types
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:gpa/presentation/home/home_screen.dart';
+import 'package:gpa/presentation/resources/color_manager.dart';
 
 class chatbot_screen extends StatefulWidget {
   const chatbot_screen({super.key});
@@ -11,6 +16,30 @@ class chatbot_screen extends StatefulWidget {
 class _chatbot_screenState extends State<chatbot_screen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: ColorManager.white,
+        leading: IconButton(
+          onPressed: () => Get.to(const HomeScreen()),
+          icon: const Icon(
+            Icons.arrow_back_rounded,
+            color: Color.fromARGB(255, 15, 89, 149),
+            size: 32,
+          ),
+        ),
+        title: const Text(
+          'مرحبًا, أنا الكابتن نامق',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 15, 89, 149)),
+          // Align the text in the center
+        ),
+        toolbarHeight: 90.0,
+      ),
+      // body: ,
+    );
   }
 }
--- lib/presentation/chatbot/chatbot_welcome_screen.dart
+++ lib/presentation/chatbot/chatbot_welcome_screen.dart
@@ -14,41 +14,71 @@ class _chatbo_welcomeState extends State<chatbo_welcome> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/Sign_up_page.png"),
-                fit: BoxFit.fill)),
-        child: Column(
-          children: [
-            const Text('مرحبًا, أنا نامق'),
-            Container(
-              width: 150,
-              height: 150,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: AssetImage('assets/images/captine.jpg'),
-                  fit: BoxFit.cover,
+      child: Scaffold(
+        body: Container(
+          padding: EdgeInsets.fromLTRB(50, 20, 50, 10),
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/Sign_up_page.png"),
+                  fit: BoxFit.fill)),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 110),
+              const Text(
+                'مرحبًا, أنا نامق',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+                textDirection: TextDirection.rtl,
+              ),
+              const SizedBox(height: 75),
+              Container(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        // padding: const EdgeInsets.only(left: 15, right: 50),
+                        margin: const EdgeInsets.only(bottom: 10),
+                        width: 150,
+                        height: 150,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/captine.jpg'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const Center(
+                        child: Text(
+                          'هنا لمساعدتك على إيجاد إجابات لتساؤلاتك حول الخدمات الأكديمية وما يخص الجامعة ',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ],
+                  )),
+              const SizedBox(height: 80),
+              TextButton(
+                style: TextButton.styleFrom(
+                    foregroundColor: ColorManager.white,
+                    backgroundColor: ColorManager.primary,
+                    // padding: EdgeInsets.fromLTRB(left, top, right, bottom),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                onPressed: () {
+                  Get.to(() => const chatbot_screen());
+                  print('clicked');
+                },
+                child: const Text(
+                  'دعنا نتحدث!',
+                  style: TextStyle(fontSize: 25),
                 ),
-              ),
-            ),
-            const Text(
-                'here to help answer your questions regarding university services!'),
-            TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: ColorManager.white,
-                backgroundColor: ColorManager.primary,
-              ),
-              onPressed: () {
-                () => Get.to(() => const chatbot_screen());
-              },
-              child: const Text('دعنا نتحدث!'),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
