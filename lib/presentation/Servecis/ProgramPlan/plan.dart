e8d658817ddf12365d1460ca091f9d2f5c856ded
Joury <jours-222@outook.com>
2024-04-16 12:25:07 +0300

    create about, stdplan, and advisor pages, with backbone style

--- lib/presentation/Servecis/ProgramPlan/plan.dart
+++ lib/presentation/Servecis/ProgramPlan/plan.dart
@@ -1,4 +1,9 @@
+// ignore_for_file: camel_case_types
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:gpa/presentation/resources/color_manager.dart';
 
 class plan extends StatefulWidget {
   const plan({super.key});
@@ -10,6 +15,31 @@ class plan extends StatefulWidget {
 class _planState extends State<plan> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+      elevation: 0,
+       toolbarHeight: 90,
+      title: Text(
+            "student plan".tr,
+            textAlign: TextAlign.center,
+            style: const TextStyle(color: Colors.white, fontSize: 50), // Align the text in the center
+          ),
+        centerTitle: true,
+      backgroundColor: ColorManager.primary,
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back),
+        color: Colors.white,
+        onPressed: () {
+          Navigator.of(context).pop(); // This will navigate back to the previous screen
+        },
+      ),
+      actions: [],
+      ),
+
+      body: Container(
+        foregroundDecoration: const BoxDecoration(color: Colors.white),
+      ),
+    );
   }
+
 }
